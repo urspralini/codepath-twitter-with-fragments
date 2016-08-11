a3b7ee5cb647ca80a76225a8c1688723b5027abb
pbabu <prabhu.babu@elementum.com>
2016-08-11 07:47:53 -0700

    added view pager and pagerSlidingTabString code

--- app/src/main/java/com/codepath/apps/twitter/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/twitter/activities/TimelineActivity.java
@@ -2,16 +2,22 @@ package com.codepath.apps.twitter.activities;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.astuetz.PagerSlidingTabStrip;
 import com.bumptech.glide.Glide;
 import com.codepath.apps.twitter.R;
 import com.codepath.apps.twitter.RestApplication;
 import com.codepath.apps.twitter.clients.TwitterClient;
 import com.codepath.apps.twitter.databinding.ActivityTimelineBinding;
+import com.codepath.apps.twitter.fragments.HomeTimeLineFragment;
+import com.codepath.apps.twitter.fragments.MentionsTimeLineFragment;
 import com.codepath.apps.twitter.models.User;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -29,19 +35,40 @@ public class TimelineActivity extends AppCompatActivity {
     private TwitterClient mClient = RestApplication.getRestClient();
     private ActivityTimelineBinding mBinding;
     private User mCurrentUser;
+    private ViewPager mVPager;
+    private PagerSlidingTabStrip mTabs;
+    private FragmentPagerAdapter mPagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_timeline);
-        mToolBar = mBinding.appbar.toolbar;
-        mToolBarTitle = mBinding.appbar.tvToolbarTitle;
-        mIvToolBarImage = mBinding.appbar.ivUserImage;
-        setSupportActionBar(mToolBar);
-        //Display icon in the toolbar
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-        // Remove default title text
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        getCurrentUser();
+        setupToolBar();
+        mVPager = mBinding.vPager;
+        mPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            private String tabTitles[] = new String[] {"Home", "Mentions"};
+            @Override
+            public Fragment getItem(int position) {
+                if(position == 0) {
+                    return new HomeTimeLineFragment();
+                }else if (position == 1) {
+                    return new MentionsTimeLineFragment();
+                }
+                return null;
+            }
+
+            @Override
+            public int getCount() {
+                return tabTitles.length;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return tabTitles[position];
+            }
+        };
+        mVPager.setAdapter(mPagerAdapter);
+        mTabs = mBinding.tabs;
+        mTabs.setViewPager(mVPager);
     }
 
     private void getCurrentUser() {
@@ -80,5 +107,17 @@ public class TimelineActivity extends AppCompatActivity {
                 super.onFailure(statusCode, headers, throwable, errorResponse);
             }
         });
+    }
+
+    private void setupToolBar() {
+        mToolBar = mBinding.appbar.toolbar;
+        mToolBarTitle = mBinding.appbar.tvToolbarTitle;
+        mIvToolBarImage = mBinding.appbar.ivUserImage;
+        setSupportActionBar(mToolBar);
+        //Display icon in the toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        // Remove default title text
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getCurrentUser();
     }
 }
