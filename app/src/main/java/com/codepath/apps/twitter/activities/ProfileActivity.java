5d7813ed2dc6bb63fe5b03ce868b18185dd4bffa
pbabu <prabhu.babu@elementum.com>
2016-08-13 21:14:23 -0700

    customizing UI for profile activity

--- app/src/main/java/com/codepath/apps/twitter/activities/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/twitter/activities/ProfileActivity.java
@@ -7,20 +7,22 @@ import android.widget.TextView;
 import com.codepath.apps.twitter.R;
 import com.codepath.apps.twitter.fragments.ProfileHeaderFragment;
 import com.codepath.apps.twitter.fragments.UserTimeLineFragment;
+import com.codepath.apps.twitter.models.User;
 
-public class ProfileActivity extends BaseActivity {
+public class ProfileActivity extends BaseActivity implements ProfileHeaderFragment.UserInfoListener{
 
     public static final String SCREEN_NAME_KEY = "screen_name";
     private Toolbar mToolBar;
     private TextView mToolBarTitle;
+    private User mUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         String screenName = getIntent().getStringExtra(SCREEN_NAME_KEY);
-        setupToolBar(screenName);
+        setupToolBar();
         UserTimeLineFragment userTimeLineFragment = UserTimeLineFragment.newInstance(screenName);
-        ProfileHeaderFragment profileHeaderFragment = ProfileHeaderFragment.newInstance(screenName);
+        ProfileHeaderFragment profileHeaderFragment = ProfileHeaderFragment.newInstance(screenName, this);
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.flUserTimeLine, userTimeLineFragment)
@@ -28,14 +30,24 @@ public class ProfileActivity extends BaseActivity {
                 .commit();
     }
 
-    private void setupToolBar(String screenName) {
+    private void setupToolBar() {
         mToolBar = (Toolbar)findViewById(R.id.toolbar);
         mToolBarTitle = (TextView)findViewById(R.id.tvToolbarTitle);
         setSupportActionBar(mToolBar);
         //Display icon in the toolbar
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_arrow);
         // Remove default title text
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-        mToolBarTitle.setText("@" + screenName);
+    }
+
+    @Override
+    public void sendUser(User user) {
+        mUser = user;
+        updateToolBarTitle(user.getName());
+    }
+
+    private void updateToolBarTitle(String text) {
+        mToolBarTitle.setText(text);
     }
 }
--- app/src/main/java/com/codepath/apps/twitter/fragments/ProfileHeaderFragment.java
+++ app/src/main/java/com/codepath/apps/twitter/fragments/ProfileHeaderFragment.java
@@ -30,12 +30,18 @@ public class ProfileHeaderFragment extends Fragment {
 
     public static final String SCREEN_NAME_KEY = "screen_name";
     private ImageView ivProfileImage;
+    private TextView tvName;
     private TextView tvScreenName;
     private TextView tvTagLine;
     private TextView tvFollowingCount;
     private TextView tvFriendsCount;
     private TextView tvFollowing;
     private TextView tvFriends;
+    private UserInfoListener mUserInfoListener;
+
+    public interface UserInfoListener {
+        void sendUser(User user);
+    }
 
     private JsonHttpResponseHandler handler = new JsonHttpResponseHandler() {
         @Override
@@ -46,12 +52,14 @@ public class ProfileHeaderFragment extends Fragment {
                 Picasso.with(getActivity())
                         .load(mUser.getProfileImageUrl())
                         .into(ivProfileImage);
-                tvScreenName.setText(mUser.getScreenName());
+                tvName.setText(mUser.getName());
+                tvScreenName.setText("@"+mUser.getScreenName());
                 tvTagLine.setText(mUser.getTagLine());
                 tvFollowing.setText(R.string.following);
                 tvFriends.setText(R.string.followers);
                 tvFriendsCount.setText(String.valueOf(mUser.getFriendsCount()));
                 tvFollowingCount.setText(String.valueOf(mUser.getFollowingCount()));
+                mUserInfoListener.sendUser(mUser);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -77,11 +85,12 @@ public class ProfileHeaderFragment extends Fragment {
     private User mUser;
     private TwitterClient mClient = RestApplication.getRestClient();
 
-    public static ProfileHeaderFragment newInstance(String screenName) {
+    public static ProfileHeaderFragment newInstance(String screenName, UserInfoListener listener) {
 
         Bundle args = new Bundle();
         args.putString(SCREEN_NAME_KEY, screenName);
         ProfileHeaderFragment fragment = new ProfileHeaderFragment();
+        fragment.mUserInfoListener = listener;
         fragment.setArguments(args);
         return fragment;
     }
@@ -98,6 +107,7 @@ public class ProfileHeaderFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         final View view = inflater.inflate(R.layout.fragment_profile_header, container, false);
         ivProfileImage = (ImageView)view.findViewById(R.id.ivProfileImage);
+        tvName = (TextView)view.findViewById(R.id.tvName);
         tvScreenName = (TextView)view.findViewById(R.id.tvScreenName);
         tvTagLine = (TextView)view.findViewById(R.id.tvTagLine);
         tvFollowing = (TextView)view.findViewById(R.id.tvFollowing);
